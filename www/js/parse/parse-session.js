cbaaa360cf7f0b8b2e98fd52778df66e1574c241
JoeAzar <jazar@acm.org>
2015-09-06 07:17:00 -0400

    ch-ch-ch-changes

--- www/js/parse/parse-session.js
+++ www/js/parse/parse-session.js
@@ -1,11 +1,11 @@
 Parse.initialize("tnT3xM2pzKzrLhqzSxtBMEV5z8LOk93qgtqvwubw", "fuZTNH963qimEXgzCFXjt8S1o24UpcOUZFJeA5V5");
 
-var currentUser = Parse.User.current();
-if (currentUser) {
-    // do stuff with the user
-} else {
-    window.location = ("index.html");
-}
+//var currentUser = Parse.User.current();
+//if (currentUser) {
+//    // do stuff with the user
+//} else {
+//    window.location = ("index.html");
+//}
 
 function logout(){
     Parse.User.logOut();
